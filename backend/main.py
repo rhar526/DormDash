7420cb3f1ef86ba029cf7302b74eb95f5ce8f4d7
rhar <rhar526@gmail.com>
2025-11-08 14:07:30 -0500

    app stuff

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
+@app.route("/")
+def home():
+    return "Flask is running! Go to /api/users"
 
 @app.route("/api/users", methods = ['GET'])
 def users():
@@ -15,4 +20,4 @@ def users():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True, port = 8000)
+    app.run(debug=True, port = 8080)
